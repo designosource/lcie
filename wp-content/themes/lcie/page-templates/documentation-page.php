3ee694f849dcad5451b926fe460cd2f751c189fa
Diederik Craps <diederik127@hotmail.com>
2017-01-20 13:18:02 +0100

    Documentation layout
    Still needs WP functionallity

---
+++ wp-content/themes/lcie/page-templates/documentation-page.php
@@ -0,0 +1,68 @@
+<?php
+// Template name: Documentation-Page
+?>
+
+
+<?php get_header(); ?>
+
+<section class="home__hero">
+  <div class="home__hero__overlay"></div>
+  <div class="wrapper">
+    <div class="home__hero__content">
+      <h1 class="home__hero__title">Documentatie</h1>
+      <p class="home__hero__text">Hier kan je heel wat nuttige documentatie over ondernemen raadplegen.</p>
+    </div>
+  </div>
+</section>
+
+<div class="documentation__nav">
+  <a href="#" class="documentation__nav__item documentation__nav__item__active">Case Studies</a>
+  <a href="#" class="documentation__nav__item">Video's</a>
+  <a href="#" class="documentation__nav__item">Presentaties</a>
+  <a href="#" class="documentation__nav__item">Templates</a>
+</div>
+
+<div class="documentation__article__grid">
+  <div class="documentation__article__item">
+    <h2>Case studie 1</h2>
+    <p>Dit is een case studie over Ondernemen</p>
+    <a href="">Download</a>
+  </div>
+  <div class="documentation__article__item">
+    <h2>Case studie 1</h2>
+    <p>Dit is een case studie over Ondernemen</p>
+    <a href="">Download</a>
+  </div>
+  <div class="documentation__article__item">
+    <h2>Case studie 1</h2>
+    <p>Dit is een case studie over Ondernemen</p>
+    <a href="">Download</a>
+  </div>
+  <div class="documentation__article__item">
+    <h2>Case studie 1</h2>
+    <p>Dit is een case studie over Ondernemen</p>
+    <a href="">Download</a>
+  </div>
+  <div class="documentation__article__item">
+    <h2>Case studie 1</h2>
+    <p>Dit is een case studie over Ondernemen</p>
+    <a href="">Download</a>
+  </div>
+  <div class="documentation__article__item">
+    <h2>Case studie 1</h2>
+    <p>Dit is een case studie over Ondernemen</p>
+    <a href="">Download</a>
+  </div>
+  <div class="documentation__article__item">
+    <h2>Case studie 1</h2>
+    <p>Dit is een case studie over Ondernemen</p>
+    <a href="">Download</a>
+  </div>
+  <div class="documentation__article__item">
+    <h2>Case studie 1</h2>
+    <p>Dit is een case studie over Ondernemen</p>
+    <a href="">Download</a>
+  </div>
+</div>
+
+<?php get_footer(); ?>

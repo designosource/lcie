cd832f920b7139df25c91f58ff43e1ab3f66a1eb
nickysomers <somers.nicky@gmail.com>
2017-01-30 20:36:08 +0100

    Fixed form

--- wp-content/themes/lcie/page-templates/apply-page.php
+++ wp-content/themes/lcie/page-templates/apply-page.php
@@ -16,7 +16,18 @@
 </section>
 
 <section class="apply__form">
-  <div class="apply__hook__top"></div>
+ 
+
+      <div class="wrapper offer__content__inner">
+            <svg class="hook-under" viewBox="0 0 100 100"> 
+          <path d="M20,58 L20,0 L0,0 L0,68 L0,78 L78,78 L78,58 L20,58 Z" id="Combined-Shape"></path>
+      </svg>
+
+    <svg class="hook-up" viewBox="0 0 100 100">
+          <path d="M20,58 L20,0 L0,0 L0,68 L0,78 L78,78 L78,58 L20,58 Z" id="Combined-Shape" transform="translate(39.000000, 39.000000) rotate(180.000000) translate(-39.000000, -39.000000) "></path>
+      </svg>
+
+      
 <h2 class="apply__form__title">Lcie intake formulier</h2>
   <?php $query = new WP_Query(array('page_id' => 80)); ?>
   <?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
@@ -26,7 +37,7 @@
 
 
   <?php endwhile; endif; ?>
-    <div class="apply__hook__bottom"></div>
+
 </section>
 
 

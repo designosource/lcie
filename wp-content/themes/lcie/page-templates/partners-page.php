85628a88e8e91ea1ce51ae990534f44440b02846
Diederik Craps <diederik127@hotmail.com>
2017-02-11 12:43:27 +0100

    Partners theming done
    Still needs WP functionality

---
+++ wp-content/themes/lcie/page-templates/partners-page.php
@@ -0,0 +1,75 @@
+<?php
+// Template name: Partners-Page
+?>
+
+
+<?php get_header(); ?>
+
+<section class="home__hero">
+  <div class="home__hero__overlay"></div>
+  <div class="wrapper">
+    <div class="home__hero__content">
+      <h1 class="home__hero__title">Partners</h1>
+      <p class="home__hero__text">Partners van Lcie.</p>
+    </div>
+  </div>
+</section>
+
+<div class="wrapper">
+  <p>De Leuven Community for Innovation driven Entrepreneurship (LCIE) heeft
+    relaties uitgebouwd met meerdere geassocieerde partners. Deze partners delen
+  dezelfde opvatting over de nood aan innovatie, creativiteit en ondernemerschap
+  die de maatschappij van vandaag en morgen ten goede moet komen.</p>
+
+  <h2>Powered By</h2>
+  <section class="partners__grid">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="wp-content/themes/lcie/images/lcie.png" alt="" class="partners__grid__item">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="wp-content/themes/lcie/images/lcie.png" alt="" class="partners__grid__item">
+    <img src="wp-content/themes/lcie/images/lcie.png" alt="" class="partners__grid__item">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="wp-content/themes/lcie/images/lcie.png" alt="" class="partners__grid__item">
+  </section>
+
+  <h2>In partnership with</h2>
+  <section class="partners__grid">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="wp-content/themes/lcie/images/lcie.png" alt="" class="partners__grid__item">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="wp-content/themes/lcie/images/lcie.png" alt="" class="partners__grid__item">
+    <img src="wp-content/themes/lcie/images/lcie.png" alt="" class="partners__grid__item">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="wp-content/themes/lcie/images/lcie.png" alt="" class="partners__grid__item">
+  </section>
+
+  <h2>Sponsored by</h2>
+  <section class="partners__grid">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="wp-content/themes/lcie/images/lcie.png" alt="" class="partners__grid__item">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="wp-content/themes/lcie/images/lcie.png" alt="" class="partners__grid__item">
+    <img src="wp-content/themes/lcie/images/lcie.png" alt="" class="partners__grid__item">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="wp-content/themes/lcie/images/lcie.png" alt="" class="partners__grid__item">
+  </section>
+
+  <h2>IusStart Partners</h2>
+  <section class="partners__grid">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+    <img src="https://faber.kuleuven.be/nl/pv/pvg/pvg-oedeemtherapie/ku-leuven" alt="" class="partners__grid__item">
+  </section>
+
+</div>
+
+
+<?php get_footer(); ?>

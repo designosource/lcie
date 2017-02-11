78d8fd36d28141e1dcb199dc0f62e6fd73bcfb39
Diederik Craps <diederik127@hotmail.com>
2017-02-11 15:04:33 +0100

    Contests Theming (desktop+tablet+mobile)
    Still needs Wordpress functionality

---
+++ wp-content/themes/lcie/page-templates/contests-page.php
@@ -0,0 +1,71 @@
+<?php
+// Template name: Contests-Page
+?>
+
+
+<?php get_header(); ?>
+
+<section class="home__hero">
+  <div class="home__hero__overlay"></div>
+  <div class="wrapper">
+    <div class="home__hero__content">
+      <h1 class="home__hero__title">Wedstrijden</h1>
+      <p class="home__hero__text">Wedstijden bieden de mogelijkheid om jouw
+        businessplan te verbeteren en in contact te komen met gelijkgezinde personen,
+      ondernemers en investeerders.</p>
+    </div>
+  </div>
+</section>
+
+<div class="wrapper">
+  <p>Onderzoekers van de Associatie KU Leuven die aan zulke wedstrijden willen
+    deelnemen, contacteren best KU Leuven Research &
+Development alvorens deel te nemen, zodat mogelijke problemen met betrekking
+tot intellectuele eigendom tijdig kunnen worden
+nagekeken. Studenten die een idee uitwerken, beschikken meestal zelf over hun
+intellectuele eigendom, behalve wanneer gebruik wordt
+gemaakt van technologie van de KU Leuven. In dit laatste geval nemen ook zij
+best contact op met KU Leuven Research & Development.</p>
+
+
+<h2>Lopende wedstrijden</h2>
+
+<section class="contests__grid">
+  <article class="contests__grid__item">
+    <div class="contests__grid__item__haakBoven"></div>
+    <img src="http://www.startacademy.be/wp-content/uploads/2015/09/Logo_start_2015-e1441359012930.png" alt="" class="contests__item__img">
+    <h3>Start Academy</h3>
+    <p>Start Academy is zowel een academie als
+      een wedstrijd. Je start met een subliem
+      idee dat kritisch wordt bekeken door een
+      jury. Tijd dus om een en ander op papier
+      te zetten. Maar geen stress want tijdens
+      de workshops word je hiervoor begeleid
+      door professionals en je kan steeds
+      beroep doen op onze coaches.</p>
+<a href="#" class="contests__item__link">Meer over deze wedstrijd.</a>
+<div class="contests__grid__item__haakOnder"></div>
+  </article>
+  <article class="contests__grid__item">
+    <div class="contests__grid__item__haakBoven"></div>
+    <img src="http://www.startacademy.be/wp-content/uploads/2015/09/Logo_start_2015-e1441359012930.png" alt="" class="contests__item__img">
+    <h3>Health Axis Accelerator</h3>
+    <p>The HAE is a pan-European program to
+identify, train and connect the best health
+technology start-up teams from
+innovation hubs in Europe with venture
+capital funds. The program is open for
+participants from the HAE partner clusters
+Cambridge (UK), Leuven (Belgium) and
+Heidelberg (Germany).</p>
+<a href="#" class="contests__item__link">Meer over deze wedstrijd.</a>
+<div class="contests__grid__item__haakOnder"></div>
+  </article>
+  
+</section>
+
+
+</div>
+
+
+<?php get_footer(); ?>

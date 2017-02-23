f117efea75c3e8f3d918805c9565721ac60ad03e
diederikminsky <diederik@minsky.be>
2017-02-23 15:09:41 +0100

    THEMING - Fixed Teams Page

--- wp-content/themes/lcie/header.php
+++ wp-content/themes/lcie/header.php
@@ -1,24 +1,24 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/dist/css/style.min.css">
+		<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/style.css">
 
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	
+
 		<?php if(is_archive()): ?>
 			<title>FAQ | Lcie</title>
 		<?php else: ?>
 			<title><?php the_title(); ?> | Lcie</title>
 		<?php endif; ?>
-		
-	
+
+
 		<?php wp_head(); ?>
 	</head>
 	<body <?php body_class(); ?>>
 
 		<header class="header">
-			
+
 			<nav class="header__subnavigation">
 				<div class="wrapper">
 					<ul>
--- wp-content/themes/lcie/page-templates/teams-page.php
+++ wp-content/themes/lcie/page-templates/teams-page.php
@@ -6,7 +6,7 @@
 <?php get_header(); ?>
 
 
-	<section class="page__top" style="background-image: url(<?php the_field("header_image"); ?>);"> 
+	<section class="page__top" style="background-image: url(<?php the_field("header_image"); ?>);">
 		<div class="wrapper">
 			<h1 class="page__top__title">Teams</h1>
 			<h2 class="page__top__subtitle">bekijk hieronder onze teams</h2>
@@ -15,23 +15,23 @@
 	</section>
 
 	<section class="teams__content">
-		
+
 		<div class="wrapper">
-			
+
 			<h1>Lopende projecten</h1>
 
 			<?php $query = new WP_Query(array('post_type' => "team", "year" => date("Y"))); ?>
 			<?php if ( $query->have_posts() ) : while ( $query->have_posts() ) : $query->the_post(); ?>
 					<div class="grid teams__content__grid">
-				
+
 						<div class="teams__content__grid__col">
-							
+
 							<img src="<?php the_field("logo"); ?>" alt="<?php the_title(); ?>" class="teams__content__grid__col__image">
 
 						</div>
 
 						<div class="teams__content__grid__col">
-							
+
 							<p><?php the_field("short_text"); ?></p>
 
 							<a href="<?php the_permalink(); ?>" class="teams__content__grid__col__more">lees meer</a>
@@ -44,7 +44,7 @@
 				</section>
 			<section class="teams__pastprojects">
 			<div class="wrapper">
-			<h1>Afgelopen projecten</h1>
+			<a href="" class="teams__pastprojects__expand">Afgelopen projecten</a>
 			<?php $query = new WP_Query(array('post_type' => "team")); ?>
 			<?php
 				$years = array();
@@ -58,7 +58,7 @@
 				      	if ( ! isset( $years[ $year ]  )) $years[ $year ] = array();
 				      $years[ $year ][] = array( 'title' => get_the_title(), 'permalink' => get_the_permalink(), 'logo' => get_field("logo") );
 				      }
-				      
+
 				    }
 				}
 
@@ -67,19 +67,19 @@
 				<h2 class="teams__pastprojects__year"><?php echo $key; ?></h2>
 				<div class="grid teams__pastprojects__grid">
 				<?php foreach($year as  $year2): ?>
-					
-					
 
-						
+
+
+
 							<div class="teams__pastprojects__grid__col">
 								<a href="<?php echo $year2["permalink"]; ?>">
 									<img src="<?php echo $year2["logo"]; ?>" alt="" class="teams__pastprojects__grid__col__logo">
 								</a>
 							</div>
-						
 
-		
-				
+
+
+
 				<?php endforeach; ?>
 		</div>
 			<?php endforeach; ?>
@@ -90,6 +90,23 @@
 
 
 
-
+<script type="text/javascript">
+var pastProjectsOpen = false;
+	jQuery(document).ready(function(){
+			jQuery('.teams__pastprojects__year').hide();
+			jQuery('.teams__pastprojects__grid').hide();
+			pastProjectsOpen = false;
+	});
+	jQuery('.teams__pastprojects__expand').click(function(){
+		if (pastProjectsOpen){
+			jQuery('.teams__pastprojects__year').hide();
+			jQuery('.teams__pastprojects__grid').hide();
+		}
+		else {
+			jQuery('.teams__pastprojects__year').show();
+			jQuery('.teams__pastprojects__grid').show();
+		}
+	});
+</script>
 
 <?php get_footer(); ?>

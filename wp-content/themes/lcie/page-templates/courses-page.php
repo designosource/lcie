130d88a4149aff009e4ec1f0625cb6a4cb89f7de
diederikcraps <diederik127@hotmail.com>
2017-03-02 11:43:15 +0100

    THEMING - Courses

---
+++ wp-content/themes/lcie/page-templates/courses-page.php
@@ -0,0 +1,38 @@
+<?php
+// Template name: Courses-Page
+?>
+
+
+<?php get_header(); ?>
+
+<?php get_template_part( '/template-parts/page', 'header' ); ?>
+
+
+
+<section class="courses__content">
+<div class="courses__intro">
+  <nav class="courses__sidebar">
+    <ul>
+      <li class="courses__sidebar__active">Lcie Academy</li>
+      <li><a href="#">Stagevakken</a></li>
+      <li><a href="#">PIP</a></li>
+    </ul>
+  </nav>
+
+  <div class="courses__intro__text">
+    <h1>Lcie Entrepreneurship Academy</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Recusandae, repudiandae veritatis sapiente perferendis beatae iusto necessitatibus quos aliquid! Neque ullam, velit iusto nemo. Fuga et sunt, commodi quo, totam praesentium?</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat voluptate similique necessitatibus laudantium ipsam inventore quam sapiente at, dignissimos distinctio, provident quibusdam veritatis suscipit beatae mollitia. Enim nobis saepe harum.</p>
+  </div>
+</div>
+
+
+<h2>Programme Objectives</h2>
+<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi, perspiciatis. Eos doloribus eveniet, optio, aut deleniti quo harum repudiandae! Quae tempore non impedit cumque deserunt velit asperiores autem, ipsam illo.</p>
+<p>Deserunt nobis maxime quidem sequi blanditiis ab cum repudiandae, ipsum repellendus voluptatum accusantium ea ratione similique quo assumenda aliquam laborum. Eum qui quasi voluptas nihil velit, dicta et distinctio, ducimus.</p>
+<p>Est perspiciatis excepturi optio animi fuga, velit aut suscipit delectus, dicta voluptatibus rem reiciendis debitis et, maiores dolore quae sunt! Quae iusto est quaerat accusantium doloribus molestiae voluptate dignissimos non.</p>
+</section>
+
+
+
+<?php get_footer(); ?>
